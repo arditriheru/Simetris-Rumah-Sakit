571524ba5ed0c2fd3b0e1da002bfdd008253444d
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-25 11:54:42 +0700

    Add files via upload

---
+++ app/inventaris/views/footer.php
@@ -0,0 +1,14 @@
+<br><br><?php include "../../system/copyright.php";?><br><br>
+</div>
+<!-- JavaScript -->
+<script src="../../vendors/js/jquery-1.10.2.js"></script>
+<script src="../../vendors/js/bootstrap.js"></script>
+<script src="../../vendors/js/sweetalert.min.js"></script>
+<!-- Page Specific Plugins -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+<script src="../../vendors/js/morris/chart-data-morris.js"></script>
+<script src="../../vendors/js/tablesorter/jquery.tablesorter.js"></script>
+<script src="../../vendors/js/tablesorter/tables.js"></script>
+</body>
+</html>
