26f10445baf53a69ba2a4599e4b045181de048ce
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-25 12:52:32 +0700

    Add files via upload

---
+++ app/lite/views/footer.php
@@ -0,0 +1,28 @@
+<div align="center"><p>Developed by Ardi Tri Heru<br>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script><a expr:href='data:blog.homepageUrl'><data:blog.title/>.</a> All rights reserved.<br><font face="consolas" >Version 1.0.Beta</font></div><br><br>
+</div><!-- Right Panel -->
+<!-- footer -->
+<script>
+      // Loading Page
+      var myVar;
+      function myFunction() {
+        myVar = setTimeout(showPage, 500);
+      }
+      function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+      }
+      $(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+          });
+      });
+   
+    </script>
+    <script src="vendors/sweetalert/sweetalert.min.js"></script>
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/dashboard.js"></script>
+  </body>
+  </html>
