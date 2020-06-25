4760ca8fb3a1292ad861e3902ec90439dd107f0c
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-25 13:03:33 +0700

    Create header.php

---
+++ app/sdm/views/header.php
@@ -0,0 +1,25 @@
+<?php include "../session-start.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SIMETRIS - Sumber Daya Manusia</title>
+  <!-- Bootstrap core CSS -->
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+  <!-- Add custom CSS here -->
+  <link href="../css/sb-admin.css" rel="stylesheet">
+  <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+  <!-- Page Specific CSS -->
+  <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+  <script type="text/javascript" src="chartjs/Chart.js"></script>
+</head>
+<body>
+  <nav>
+    <div id="wrapper">
+      <?php include "menu.php"; ?>
+    </div><!-- /.navbar-collapse -->
+  </nav>
