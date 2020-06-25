62168ad1d38d065ad0ba414204926b97b99c552a
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-25 11:48:10 +0700

    Create header.php

---
+++ app/booking/views/header.php
@@ -0,0 +1,39 @@
+<?php error_reporting(0); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SIMETRIS | Booking</title>
+    <!-- Bootstrap core CSS -->
+    <link href="../../vendors/css/bootstrap.css" rel="stylesheet">
+    <!-- Add custom CSS here -->
+    <link href="../../vendors/css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../vendors/css/sweetalert.css">
+    <style>
+        .bluetext {
+            color: #008cba;
+        }
+        .redtext {
+            color: #e71414;
+        }
+        .navbar-rachmi{
+            background-color:#e67e22;
+            border-color:#d35400
+        }
+        .navbar-brand{
+            color:#ffffff;
+        }
+
+    </style>
+</head>
+<?php
+include '../../config/connect.php';
+date_default_timezone_set("Asia/Jakarta");
+$tanggalsekarang    =   date('Y-m-d');
+$jamsekarang        =   date("h:i:sa");
+?>
+
