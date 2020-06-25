2806d3b8c2ec6abf6671bc195dfdbfaec0c988ca
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-25 13:12:25 +0700

    Create copyright.php

---
+++ system/copyright.php
@@ -0,0 +1,12 @@
+<!--
+	Name        : SIMETRIS
+	Description : Sistem Informasi Rumah Sakit.
+	Date        : 2019
+	Developer   : Ardi Tri Heru
+	Phone/WA    : 0896-2967-1717
+	Email       : arditriheruh@gmail.com
+	Website     : https://www.arditriheru.com
+-->
+<div align="center"><b>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></b> All Right Reserved.  
+<br><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <a href="https://instagram.com/arditriheru"><b class="bluetext">Tim Teknologi Informasi</b></a><br>Long Term Support<br><font face="consolas" >Version 5.0</font></div>
+<br><br>
