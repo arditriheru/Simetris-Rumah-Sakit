233f49b0eff6ca9eb8f913485fd3c1226f9ed86e
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-03 11:40:23 +0700

    Add files via upload

--- app/inventaris/login.php
+++ app/inventaris/login.php
@@ -5,31 +5,26 @@
 	<link rel="stylesheet" type="text/css" href="../../vendors/css/sweetalert.css">
 </head>
 <body>
-	<div class="kotak_login">
-		<p class="tulisan_login">Silahkan login</p>
-		<form method="post" action="">
-			<label>Username</label>
-			<input type="text" name="username" class="form_login" placeholder="Username ..">
-			<label>Password</label>
-			<input type="password" name="password" class="form_login" placeholder="Password ..">
-			<input type="submit" name="login" class="tombol_login" value="LOGIN">
-			<br/>
-			<br/>
-		</form>
-		<a href="../">Kembali</a>
-	</div>
 	<?php
 	if (isset($_POST['login'])){
+		session_start();
+		include '../../config/connect.php';
 		$username = $_POST['username'];
 		$password = $_POST['password'];
+		$data = mysqli_query($koneksi,"SELECT * FROM admin WHERE username='$username' AND password='$password'");
+		while($d = mysqli_fetch_array($data)){
+			$nama = $d['nama'];
+		}
+		$cek = mysqli_num_rows($data);
 
-		if($username == 'psrs.rskiarachmi@gmail.com' && $password == 'wachidhasyim47'){
+		if($cek > 0){
 			$_SESSION['username'] = $username;
+			$_SESSION['status'] = "login";
 			echo "<script>
 			setTimeout(function() {
 				swal({
-					title: 'Uyeah!!',
-					text: 'Berhasil Login',
+					title: 'Sukses',
+					text: 'Selamat Datang $nama',
 					type: 'success'
 					}, function() {
 						window.location = 'dashboard';
@@ -40,8 +35,8 @@
 						echo "<script>
 						setTimeout(function() {
 							swal({
-								title: 'Tetot!!',
-								text: 'Username atau Password Salah!',
+								title: 'Gagal',
+								text: 'Password / Username Salah',
 								type: 'error'
 								}, function() {
 									window.location = 'login';
@@ -51,6 +46,18 @@
 								}
 							}
 							?>
+							<div class="kotak_login">
+								<p class="tulisan_login">Silahkan login</p>
+								<form method="post">
+									<label>Username</label>
+									<input type="text" name="username" class="form_login" placeholder="Masukkan ..">
+									<label>Password</label>
+									<input type="password" name="password" class="form_login" placeholder="Masukkan ..">
+									<input type="submit" class="tombol_login" name="login" value="LOGIN">
+									<br><br>			
+								</form>
+								<a href="../">Kembali</a>
+							</div>
 							<script src="../../vendors/js/sweetalert.min.js"></script>
 						</body>
 						</html>
