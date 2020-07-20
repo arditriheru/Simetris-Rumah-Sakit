1a8f7b42a8eea9306063e20086a4bf1660a9a33e
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-20 15:13:41 +0700

    Add files via upload

--- system/session-start.php
+++ system/session-start.php
@@ -1,7 +1,9 @@
 <?php 
 session_start();
 if(!isset($_SESSION['username'])) {
-	header('location:login'); }
-	else { $username = $_SESSION['username']; 
+	header('location:login');
+}else{ 
+	$username = $_SESSION['username'];
+	$nama_login = $_SESSION['nama_login']; 
 }
 ?>
