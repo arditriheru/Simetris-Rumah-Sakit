60e1e1dd66bf914603de1641d19a4e9cfb001215
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-21 14:13:55 +0700

    Add files via upload

---
+++ app/covid/operator/operator-covid-rapid-tampil.php
@@ -0,0 +1,87 @@
+<div class="row">
+  <div class="col-lg-12">
+    <div class="table-responsive">
+      <div class="row">
+        <form method="post" action="operator-covid-rapid-cari-tampil" role="form">
+          <div class="col-lg-6">
+            <div class="form-group input-group">
+              <input type="text" class="form-control" name="id_catatan_medik" placeholder="Pencarian..">
+              <span class="input-group-btn">
+                <button type="submit" class="btn btn-success"><i class="fa fa-search"></i> Cari</button>
+              </span>
+            </div>
+          </div>
+        </form>
+        <div class="col-lg-6">
+          <button name="next" type="submit" 
+          class="btn btn-primary" onClick="window.location.reload()">Refresh</button>
+        </div>
+        <div align="right" class="col-lg-12">
+          <?php
+          $m = 31;
+          $n = 7;
+          $nextN = mktime(0, 0, 0, date("m"), date("d") + $m, date("Y"));
+          $prevN = mktime(0, 0, 0, date("m"), date("d") - $n, date("Y"));
+          $mak   = date("Y-m-d", $nextN);
+          $min   = date("Y-m-d", $prevN);
+
+          $data = mysqli_query($koneksi,
+            "SELECT COUNT(id_rapidtest) AS total
+            FROM rapidtest
+            WHERE tanggal BETWEEN '$min' AND '$mak';");
+          while($d = mysqli_fetch_array($data)){
+            $total = $d['total'];
+            ?>
+            <h1><small>Total <?php echo $total; }?> Pasien</small></h1>
+          </div>
+        </div>
+        <table class="table table-bordered table-hover table-striped tablesorter">
+          <thead>
+            <tr>
+             <th><center>#</center></th>
+             <th><center>No. RM</center></th>
+             <th><center>Nama Pasien</center></th>
+             <th><center>Dokter</center></th>
+             <th><center>IgM</center></th>
+             <th><center>IgG</center></th>
+             <th><center>Register</center></th>
+           </tr>
+         </thead>
+         <tbody>
+          <?php
+          $no=$total;
+          $data = mysqli_query($koneksi,
+            "SELECT *, mr_dokter.nama_dokter,
+            CASE
+            WHEN rapidtest.igm='0' THEN 'Non Reaktif'
+            WHEN rapidtest.igm='1' THEN 'Reaktif'
+            WHEN rapidtest.igm='3' THEN 'On Process'
+            END AS nama_igm,
+            CASE
+            WHEN rapidtest.igg='0' THEN 'Non Reaktif'
+            WHEN rapidtest.igg='1' THEN 'Reaktif'
+            WHEN rapidtest.igg='3' THEN 'On Process'
+            END AS nama_igg
+            FROM rapidtest, mr_dokter
+            WHERE rapidtest.id_dokter=mr_dokter.id_dokter
+            ORDER BY rapidtest.id_rapidtest DESC");
+          while($d = mysqli_fetch_array($data)){
+            $tgl_periksa  = $d['tgl_periksa'];
+            $tanggal      = $d['tanggal'];
+            $lahir        = new DateTime($d['tgl_lahir']);
+            $today        = new DateTime();
+            $umur         = $today->diff($lahir);
+            ?>
+            <tr>
+              <td><center><?php echo $no--; ?></center></td>
+              <td><center><?php echo $d['id_catatan_medik']; ?></center></td>
+              <td><center><?php echo $d['nama']; ?></center></td>
+              <td><center><?php echo $d['nama_dokter']; ?></center></td>
+              <td><center><?php echo $d['nama_igm']; ?></center></td>
+              <td><center><?php echo $d['nama_igg']; ?></center></td>
+              <td><center><?php echo $d['tanggal'].' / '.$d['jam']; ?></center></td>
+              </tr><?php } ?>
+            </tbody>
+          </table>
+        </div>
+</div><!-- /.row -->
---
+++ app/covid/operator/operator-zona-merah.php
@@ -0,0 +1,31 @@
+<?php
+include "operator-session-start.php";
+include "../views/header.php";
+?>
+<nav>
+  <div id="wrapper">
+    <?php include "operator-menu.php"; ?>
+  </div><!-- /.navbar-collapse -->
+</nav>
+<div id="page-wrapper">
+  <div class="row">
+    <div class="col-lg-12">
+      <h1>Zona Merah <small>COVID-19</small></h1>
+      <ol class="breadcrumb">
+        <li><a href="dashboard"><i class="fa fa-dashboard"></i> Dashboard</li></a>
+        <li class="active"><i class="fa fa-exclamation-triangle"></i> Peringatan</li>
+      </ol>  
+      <?php include "../notifikasi1.php"?>
+      <div class="alert alert-warning alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        Cara memperbarui daftar zona merah COVID-19 : <b>Copy jpg / jpeg / png terbaru - explorer - network - RACHMI-SVR - folder redzone - paste - rename menjadi redzone - kembali ke simetris - CTRL+F5</b>
+      </div>
+    </div>
+    <div class="col-lg-12">
+      <img class="img-responsive" src="../../../redzone/redzone.jpg" width="100%">
+      <img class="img-responsive" src="../../../redzone/redzone.jpeg" width="100%">
+      <img class="img-responsive" src="../../../redzone/redzone.png" width="100%">
+    </div>
+  </div><!-- /#page-wrapper -->
+</div><!-- /#wrapper -->
+<?php include "../views/footer.php"; ?>
