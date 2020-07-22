b606f13c42dbb788a74922043b105ca3dbb90229
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-22 10:08:17 +0700

    Add files via upload

--- system/copyright.php
+++ system/copyright.php
@@ -8,5 +8,5 @@
 	Website     : https://www.arditriheru.com
 -->
 <div align="center"><b>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></b> All Right Reserved.  
-<br><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <a href="https://instagram.com/arditriheru"><b class="bluetext">Tim Teknologi Informasi</b></a><br>Long Term Support<br><font face="consolas" >Version 6.0</font></div>
+<br><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <a href="https://instagram.com/arditriheru"><b class="bluetext">Tim Teknologi Informasi</b></a><br>Long Term Support<br><font face="consolas" >Version 7.0</font></div>
 <br><br>
