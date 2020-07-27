0cdf2be6395d26920ed88798f50079dc93528948
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-27 13:28:53 +0700

    Add files via upload

--- app/covid/operator/operator-covid-rapid-cari-tampil.php
+++ app/covid/operator/operator-covid-rapid-cari-tampil.php
@@ -74,12 +74,20 @@ include "../views/header.php";
                   $no=1;
                   $data = mysqli_query($koneksi,
                     "SELECT *, mr_dokter.nama_dokter,
-                    IF(rapidtest.igm='1', 'Reaktif', 'Non Reaktif') AS nama_igm,
-                    IF(rapidtest.igg='1', 'Reaktif', 'Non Reaktif') AS nama_igg
+                    CASE
+                    WHEN rapidtest.igm='0' THEN 'Non Reaktif'
+                    WHEN rapidtest.igm='1' THEN 'Reaktif'
+                    WHEN rapidtest.igm='3' THEN 'On Process'
+                    END AS nama_igm,
+                    CASE
+                    WHEN rapidtest.igg='0' THEN 'Non Reaktif'
+                    WHEN rapidtest.igg='1' THEN 'Reaktif'
+                    WHEN rapidtest.igg='3' THEN 'On Process'
+                    END AS nama_igg
                     FROM rapidtest, mr_dokter
                     WHERE rapidtest.id_dokter=mr_dokter.id_dokter
-                    AND id_catatan_medik='$id_catatan_medik'
-                    ORDER BY id_rapidtest DESC;");
+                    AND rapidtest.id_catatan_medik='$id_catatan_medik'
+                    ORDER BY rapidtest.id_rapidtest DESC;");
                   while($d = mysqli_fetch_array($data)){
                     $tgl_periksa  = $d['tgl_periksa'];
                     $tanggal      = $d['tanggal'];
@@ -99,8 +107,7 @@ include "../views/header.php";
                          <th><center>Dokter</center></th>
                          <th><center>IgM</center></th>
                          <th><center>IgG</center></th>
-                         <th><center>Tanggal Input</center></th>
-                         <th colspan='3'><center>Action</center></th>
+                         <th><center>Registrasi</center></th>
                        </tr>
                      </thead>
                      <tbody>
@@ -109,26 +116,46 @@ include "../views/header.php";
                         <td><center><?php echo $d['id_catatan_medik']; ?></center></td>
                         <td><center><?php echo $d['nama']; ?></center></td>
                         <td><center><?php echo $d['nama_dokter']; ?></center></td>
-                        <td><center><?php echo $d['nama_igm']; ?></center></td>
-                        <td><center><?php echo $d['nama_igg']; ?></center></td>
+                        <td><center>
+                          <?php
+                          if($d['igm']==1){
+                            ?>
+                            <font class="redtext"><?php echo $d['nama_igm']; ?></font>
+                            <?php
+                          }elseif($d['igm']==0){
+                            ?>
+                            <font class="greentext"><?php echo $d['nama_igm']; ?></font>
+                            <?php
+                          }else{
+                            ?>
+                            <font class="blacktext"><?php echo $d['nama_igm']; ?></font>
+                            <?php
+                          }
+                          ?>
+                        </center></td>
+                        <td><center>
+                          <?php
+                          if($d['igg']==1){
+                            ?>
+                            <font class="redtext"><?php echo $d['nama_igg']; ?></font>
+                            <?php
+                          }elseif($d['igg']==0){
+                            ?>
+                            <font class="greentext"><?php echo $d['nama_igg']; ?></font>
+                            <?php
+                          }else{
+                            ?>
+                            <font class="blacktext"><?php echo $d['nama_igg']; ?></font>
+                            <?php
+                          }
+                          ?>
+                        </center></td>
                         <td><center><?php echo $d['tanggal'].' / '.$d['jam']; ?></center></td>
-                        <td>
-                          <div align="center">
-                            <a href="covid-rapid-detail?id=<?php echo $d['id_rapidtest']; ?>"
-                              <button type="button" class="btn btn-primary"><i class='fa fa-print'></i></button></a>
-                            </div>
-                          </td>
-                          <td>
-                            <div align="center">
-                              <a href="covid-rapid-edit?id=<?php echo $d['id_rapidtest']; ?>"
-                                <button type="button" class="btn btn-warning"><i class='fa fa-folder-open-o'></i></button></a>
-                              </div>
-                            </td>
-                            </tr><?php }} ?>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div><!-- /.row -->
-                </div><!-- /#wrapper -->
-                <?php include "../views/footer.php"; ?>
+                        </tr><?php }} ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div><!-- /.row -->
+            </div><!-- /#wrapper -->
+            <?php include "../views/footer.php"; ?>
