bdf8879bd117d0222c849af7d26fe56e77855a25
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-30 14:04:57 +0700

    Add files via upload

--- config/connect.php
+++ config/connect.php
@@ -1,9 +1,9 @@
 <?php 
-$connect = mysqli_connect("192.168.1.250","root","rachm12016","dbrachmi");
+$koneksi = mysqli_connect("192.168.1.250","root","rachm12016","dbrachmi");
 if (mysqli_connect_error()){echo "Database not connected : " . mysqli_connect_error();
 }
 
-// $connect = mysqli_connect("localhost","root","","dbcoba");
-// if (mysqli_connect_error()){echo "connect database gagal : " . mysqli_connect_error();
+// $koneksi = mysqli_connect("localhost","root","","dbcoba");
+// if (mysqli_connect_error()){echo "Database not connected : " . mysqli_connect_error();
 // }
 ?>
