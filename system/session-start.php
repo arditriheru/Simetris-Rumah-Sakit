e93fd8693b191557bc9062bd757376cefe2c3d06
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-30 14:20:02 +0700

    Delete session-start.php

--- system/session-start.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-if(!isset($_SESSION['username'])) {
-	header('location:login');
-}else{ 
-	$username = $_SESSION['username'];
-	$nama_login = $_SESSION['nama_login']; 
-}
-?>
