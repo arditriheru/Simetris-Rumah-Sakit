a549efe5dfb4f8aa5db2d859d10c359e76578b49
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-07-30 13:59:59 +0700

    Add files via upload

--- app/lite/index.php
+++ app/lite/index.php
@@ -1,95 +1,95 @@
 <?php include "views/header.php"; ?>
 <div class="content mt-3">
-    <div class="row">
-        <div class="col">
-            <section class="card">
-                <a href="registration">
-                    <div class="social-box color-1">
-                        <i class="fa fa-stethoscope"></i>
-                        <ul>
-                            <h5 class="blacktext">Pendaftaran</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-        <div class="col">
-            <section class="card">
-                <a href="search-schedule">
-                    <div class="social-box color-2">
-                        <i class="fa fa-calendar"></i>
-                        <ul>
-                            <h5 class="blacktext">Jadwal</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <section class="card">
-                <a href="show-room">
-                    <div class="social-box color-5">
-                        <i class="fa fa-bed"></i>
-                        <ul>
-                            <h5 class="blacktext">Bed</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-        <div class="col">
-            <section class="card">
-                <a href="show-queue">
-                    <div class="social-box color-4">
-                        <i class="fa fa-group"></i>
-                        <ul>
-                            <h5 class="blacktext">Antrian</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <section class="card">
-                <a href="price-list">
-                    <div class="social-box color-3">
-                        <i class="fa fa-list"></i>
-                        <ul>
-                            <h5 class="blacktext">Harga</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-        <div class="col">
-            <section class="card">
-                <a href="information">
-                    <div class="social-box color-a">
-                        <i class="fa fa-bullhorn"></i>
-                        <ul>
-                            <h5 class="blacktext">Info</h5>
-                        </ul>
-                    </div>
-                </a><!--/social-box-->
-            </section>
-        </div>
-    </div>
-    <nav class="nav-bottom">
-      <a href="help" class="nav-bottom__link">
-        <i class="material-icons nav-bottom__icon">help_outline</i>
-        <span class="nav-bottom__text">Help</span>
-    </a>
-    <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
-        <i class="material-icons nav__icon">dashboard</i>
-        <span class="nav-bottom__text">Home</span>
-    </a>
-    <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link">
-        <i class="material-icons nav-bottom__icon">arrow_back</i>
-        <span class="nav-bottom__text">Back</span>
-    </a>
+  <div class="row">
+    <div class="col">
+      <section class="card">
+        <a href="registration">
+          <div class="social-box color-1">
+            <i class="fa fa-stethoscope"></i>
+            <ul>
+              <h5 class="blacktext">Daftar Online</h5>
+          </ul>
+      </div>
+  </a><!--/social-box-->
+</section>
+</div>
+<div class="col">
+  <section class="card">
+    <a href="search-schedule">
+      <div class="social-box color-2">
+        <i class="fa fa-calendar"></i>
+        <ul>
+          <h5 class="blacktext">Jadwal Dokter</h5>
+      </ul>
+  </div>
+</a><!--/social-box-->
+</section>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+      <section class="card">
+        <a href="show-room">
+          <div class="social-box color-5">
+            <i class="fa fa-bed"></i>
+            <ul>
+              <h5 class="blacktext">Info Kamar</h5>
+          </ul>
+      </div>
+  </a><!--/social-box-->
+</section>
+</div>
+<div class="col">
+  <section class="card">
+    <a href="show-queue">
+      <div class="social-box color-4">
+        <i class="fa fa-group"></i>
+        <ul>
+          <h5 class="blacktext">Antrian Pasien</h5>
+      </ul>
+  </div>
+</a><!--/social-box-->
+</section>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+      <section class="card">
+        <a href="price-list">
+          <div class="social-box color-3">
+            <i class="fa fa-list"></i>
+            <ul>
+              <h5 class="blacktext">Tabel Harga</h5>
+          </ul>
+      </div>
+  </a><!--/social-box-->
+</section>
+</div>
+<div class="col">
+  <section class="card">
+    <a href="information">
+      <div class="social-box color-a">
+        <i class="fa fa-shopping-basket"></i>
+        <ul>
+          <h5 class="blacktext">Katalog</h5>
+      </ul>
+  </div>
+</a><!--/social-box-->
+</section>
+</div>
+</div>
+<nav class="nav-bottom">
+  <a href="help" class="nav-bottom__link">
+    <i class="material-icons nav-bottom__icon">help_outline</i>
+    <span class="nav-bottom__text">Help</span>
+</a>
+<a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
+    <i class="material-icons nav__icon">dashboard</i>
+    <span class="nav-bottom__text">Home</span>
+</a>
+<a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link">
+    <i class="material-icons nav-bottom__icon">arrow_back</i>
+    <span class="nav-bottom__text">Back</span>
+</a>
 </nav>
 <?php include "views/footer.php"; ?> 
--- app/lite/information.php
+++ app/lite/information.php
@@ -1,30 +1,71 @@
 <?php include "views/header.php"; ?>
 <!-- Page Content -->
 <div class="container">
-    <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Update Informasi</h1>
-    <hr class="mt-2 mb-5">
-    <div class="row text-center text-lg-left">
-        <div class="col-lg-3 col-md-4 col-6">
-            <a class="thumbnail fancybox" rel="ligthbox" href="images/information1.jpg">
-                <img class="img-fluid img-thumbnail" src="images/information1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-6">
-            <a class="thumbnail fancybox" rel="ligthbox" href="images/information2.jpg">
-                <img class="img-fluid img-thumbnail" src="images/information2.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-6">
-            <a class="thumbnail fancybox" rel="ligthbox" href="images/information3.jpg">
-                <img class="img-fluid img-thumbnail" src="images/information3.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-6">
-            <a class="thumbnail fancybox" rel="ligthbox" href="images/information4.jpg">
-                <img class="img-fluid img-thumbnail" src="images/information4.jpg" alt="">
-            </a>
-        </div>
+    <h3 class="font-weight-light text-center text-lg-left mt-4 mb-0">Katalog Tindik Steril</h3>
+    <p align="center">Tindik steril dan higienis dari <a href="https://instagram.com/dr.evoo_official"><b class="bluetext">@dr.evoo_official</b></a>. Tindik dengan standar medis tidak mengandung Nikel yang dapat menyebabkan iritasi pada kulit si kecil yang halus dan masih sensitif.<br>Pemesanan silahkan hubungi : (0274) 376717 / (0274) 415316 / 0813 9038 3000<br><a href="https://api.whatsapp.com/send?phone=6281390383000"><b class="bluetext">(Klik disini)</b></a></p>
+    <div class="content mt-3">
+      <div class="row">
+        <div class="col">
+          <section class="card">
+            <span class="badge badge-success float-right mt-1">Pre Order</span>
+    
+              <div class="social-box color-1">
+                <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-1.jpg" width="100%" alt="Koneksi internet lambat..">
+            </div>
+            <!--/social-box-->
+    </section>
+</div>
+<div class="col">
+  <section class="card">
+    <span class="badge badge-success float-right mt-1">Pre Order</span>
+      <div class="social-box color-1">
+        <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-2.jpg" width="100%" alt="Koneksi internet lambat..">
     </div>
+    <!--/social-box-->
+</section>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+      <section class="card">
+        <span class="badge badge-success float-right mt-1">Pre Order</span>
+          <div class="social-box color-1">
+            <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-3.jpg" width="100%" alt="Koneksi internet lambat..">
+        </div>
+        <!--/social-box-->
+</section>
+</div>
+<div class="col">
+  <section class="card">
+    <span class="badge badge-success float-right mt-1">Pre Order</span>
+      <div class="social-box color-1">
+        <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-4.jpg" width="100%" alt="Koneksi internet lambat..">
+    </div>
+    <!--/social-box-->
+</section>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+      <section class="card">
+        <span class="badge badge-success float-right mt-1">Pre Order</span>
+
+          <div class="social-box color-1">
+            <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-5.jpg" width="100%" alt="Koneksi internet lambat..">
+        </div>
+        <!--/social-box-->
+</section>
+</div>
+<div class="col">
+  <section class="card">
+    <span class="badge badge-success float-right mt-1">Pre Order</span>
+      <div class="social-box color-1">
+        <img class="img-responsive" src="images/rskia-rachmi-yogyakarta-tindik-steril-6.jpg" width="100%" alt="Koneksi internet lambat..">
+    </div>
+    <!--/social-box-->
+</section>
+</div>
+</div>
 </div><br>
 <!-- /.container -->
 <nav class="nav-bottom">
