8c5db30c671515c33a72bf8909b9c478fd439735
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-03 09:10:56 +0700

    Add files via upload

--- app/covid/admin/admin-session-start.php
+++ app/covid/admin/admin-session-start.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 if(!isset($_SESSION['admin_username'])) {
-	header('location:login');
+	header('location:../login');
 }else{ 
 	$username = $_SESSION['admin_username'];
 	$nama_login = $_SESSION['admin_nama_login']; 
