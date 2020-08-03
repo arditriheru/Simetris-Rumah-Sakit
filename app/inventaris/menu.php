02c7c390fd04bf4a75333e4deedf006c5cd54c05
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-03 10:03:07 +0700

    Add files via upload

--- app/inventaris/menu.php
+++ app/inventaris/menu.php
@@ -25,9 +25,6 @@
         </li>
         <li><a href="jenis-tambah"><i class="fa fa-tag"></i> Jenis</a></li>
         <li><a href="ruangan-tambah"><i class="fa fa-home"></i> Ruangan</a></li>
-        <li><a href="../../system/tutorial?id_buku=50">
-          <i class="fa fa-file"></i> Dokumentasi</a>
-        </li>
       </ul>
       <ul class="nav navbar-nav navbar-right navbar-user">
         <li class="dropdown user-dropdown">
