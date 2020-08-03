1f531a49e8751493f58fcfa417abf05627cba9b6
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-03 11:07:26 +0700

    Add files via upload

--- app/covid/covid-rapid-tambah.php
+++ app/covid/covid-rapid-tambah.php
@@ -88,12 +88,19 @@ function format_mak($mak)
           $no=$total;
           $data = mysqli_query($koneksi,
             "SELECT *, mr_dokter.nama_dokter,
-            IF(rapidtest.igm='1', 'Reaktif', 'Non Reaktif') AS nama_igm,
-            IF(rapidtest.igg='1', 'Reaktif', 'Non Reaktif') AS nama_igg
+            CASE
+            WHEN rapidtest.igm='0' THEN 'Non Reaktif'
+            WHEN rapidtest.igm='1' THEN 'Reaktif'
+            WHEN rapidtest.igm='3' THEN 'On Process'
+            END AS nama_igm,
+            CASE
+            WHEN rapidtest.igg='0' THEN 'Non Reaktif'
+            WHEN rapidtest.igg='1' THEN 'Reaktif'
+            WHEN rapidtest.igg='3' THEN 'On Process'
+            END AS nama_igg
             FROM rapidtest, mr_dokter
             WHERE rapidtest.id_dokter=mr_dokter.id_dokter
-            AND tanggal BETWEEN '$min' AND '$mak'
-            ORDER BY id_rapidtest DESC;");
+            ORDER BY rapidtest.id_rapidtest DESC");
           while($d = mysqli_fetch_array($data)){
             $tgl_periksa  = $d['tgl_periksa'];
             $tanggal      = $d['tanggal'];
@@ -153,10 +160,10 @@ function format_mak($mak)
                       <button type="button" class="btn btn-warning"><i class='fa fa-edit'></i></button></a>
                     </div>
                   </td>
-                </tr><?php } ?>
-              </tbody>
-            </table>
+                  </tr><?php } ?>
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      </div><!-- /#wrapper -->
-      <?php include "views/footer.php"; ?> 
+        </div><!-- /#wrapper -->
+        <?php include "views/footer.php"; ?> 
