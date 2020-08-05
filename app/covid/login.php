e4c92a0651431cf5474999087f0caedb772af92f
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-05 09:10:51 +0700

    Add files via upload

--- app/covid/login.php
+++ app/covid/login.php
@@ -93,9 +93,9 @@
 											<p class="tulisan_login">Silahkan login</p>
 											<form method="post">
 												<label>Username</label>
-												<input type="text" name="nama_user" class="form_login" placeholder="Masukkan ..">
+												<input type="text" name="nama_user" class="form_login" placeholder="Masukkan .." required>
 												<label>Password</label>
-												<input type="password" name="password" class="form_login" placeholder="Masukkan ..">
+												<input type="password" name="password" class="form_login" placeholder="Masukkan .." required>
 												<input type="submit" class="tombol_login" name="login" value="LOGIN">
 												<br><br>			
 											</form>
