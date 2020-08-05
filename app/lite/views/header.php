6b0abebf7a2fbd6b62aede291244ea34ef57e639
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-05 10:46:35 +0700

    Add files via upload

--- app/lite/views/header.php
+++ app/lite/views/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SIMETRIS | RSKIA Rachmi Yogyakarta</title>
+  <title>Rachmi Online | RSKIA Rachmi Yogyakarta</title>
   <meta name="description" content="Sistem Informasi Rumah Sakit RSKIA Rachmi Yogyakarta - Pendaftaran Online">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="images/favicon.ico">
