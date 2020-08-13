a5b4bc468e64d88654caacf5729675e968bb3d9c
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-13 14:42:25 +0700

    Add files via upload

--- app/kalender/index.php
+++ app/kalender/index.php
@@ -78,127 +78,136 @@ $today    =   date('Y-m-d');
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 text-center">
-				<a href="../booking/dashboard"
+				<?php
+				$id = $_GET['id'];
+				if(isset($id)){ ?>
+					<a href="../booking/dashboard"
+					<button type="button" class="btn btn-danger btn-lg btn-block"><i class="fa fa-arrow-left "></i> Kembali</button>
+				</a>
+			<?php }else{ ?>
+				<a href="javascript: history.back()"
 				<button type="button" class="btn btn-danger btn-lg btn-block"><i class="fa fa-arrow-left "></i> Kembali</button>
-			</a><br>
-			<div id="calendar" class="col-centered">
-			</div>
+			</a>
+		<?php }	?>
+		<br>
+		<div id="calendar" class="col-centered">
 		</div>
 	</div>
-	<!-- /.row -->
+</div>
+<!-- /.row -->
 
-	<!-- Modal -->
-	<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<form class="form-horizontal" method="POST" action="addEvent.php">
+<!-- Modal -->
+<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="addEvent.php">
 
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">Add Event</h4>
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Add Event</h4>
+				</div>
+				<div class="modal-body">
+
+					<div class="form-group">
+						<label for="title" class="col-sm-2 control-label">Title</label>
+						<div class="col-sm-10">
+							<input type="text" name="title" class="form-control" id="title" placeholder="Title">
+						</div>
 					</div>
-					<div class="modal-body">
+					<div class="form-group">
+						<label for="color" class="col-sm-2 control-label">Color</label>
+						<div class="col-sm-10">
+							<select name="color" class="form-control" id="color">
+								<option value="">Choose</option>
+								<option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
+								<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
+								<option style="color:#008000;" value="#008000">&#9724; Green</option>						  
+								<option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
+								<option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
+								<option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
+								<option style="color:#000;" value="#000">&#9724; Black</option>
 
-						<div class="form-group">
-							<label for="title" class="col-sm-2 control-label">Title</label>
-							<div class="col-sm-10">
-								<input type="text" name="title" class="form-control" id="title" placeholder="Title">
-							</div>
+							</select>
 						</div>
-						<div class="form-group">
-							<label for="color" class="col-sm-2 control-label">Color</label>
-							<div class="col-sm-10">
-								<select name="color" class="form-control" id="color">
-									<option value="">Choose</option>
-									<option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
-									<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
-									<option style="color:#008000;" value="#008000">&#9724; Green</option>						  
-									<option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
-									<option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
-									<option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
-									<option style="color:#000;" value="#000">&#9724; Black</option>
-
-								</select>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="start" class="col-sm-2 control-label">Start date</label>
-							<div class="col-sm-10">
-								<input type="text" name="start" class="form-control" id="start" readonly>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="end" class="col-sm-2 control-label">End date</label>
-							<div class="col-sm-10">
-								<input type="text" name="end" class="form-control" id="end" readonly>
-							</div>
-						</div>
-
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="submit" class="btn btn-primary">Save changes</button>
+					<div class="form-group">
+						<label for="start" class="col-sm-2 control-label">Start date</label>
+						<div class="col-sm-10">
+							<input type="text" name="start" class="form-control" id="start" readonly>
+						</div>
 					</div>
-				</form>
-			</div>
+					<div class="form-group">
+						<label for="end" class="col-sm-2 control-label">End date</label>
+						<div class="col-sm-10">
+							<input type="text" name="end" class="form-control" id="end" readonly>
+						</div>
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+			</form>
 		</div>
 	</div>
+</div>
 
 
 
-	<!-- Modal -->
-	<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<form class="form-horizontal" method="POST" action="editEventTitle.php">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">Edit Event</h4>
+<!-- Modal -->
+<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="editEventTitle.php">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Edit Event</h4>
+				</div>
+				<div class="modal-body">
+
+					<div class="form-group">
+						<label for="title" class="col-sm-2 control-label">Title</label>
+						<div class="col-sm-10">
+							<input type="text" name="title" class="form-control" id="title" placeholder="Title">
+						</div>
 					</div>
-					<div class="modal-body">
+					<div class="form-group">
+						<label for="color" class="col-sm-2 control-label">Color</label>
+						<div class="col-sm-10">
+							<select name="color" class="form-control" id="color">
+								<option value="">Choose</option>
+								<option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
+								<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
+								<option style="color:#008000;" value="#008000">&#9724; Green</option>						  
+								<option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
+								<option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
+								<option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
+								<option style="color:#000;" value="#000">&#9724; Black</option>
 
-						<div class="form-group">
-							<label for="title" class="col-sm-2 control-label">Title</label>
-							<div class="col-sm-10">
-								<input type="text" name="title" class="form-control" id="title" placeholder="Title">
+							</select>
+						</div>
+					</div>
+					<div class="form-group"> 
+						<div class="col-sm-offset-2 col-sm-10">
+							<div class="checkbox">
+								<label class="text-danger"><input type="checkbox"  name="delete"> Delete event</label>
 							</div>
 						</div>
-						<div class="form-group">
-							<label for="color" class="col-sm-2 control-label">Color</label>
-							<div class="col-sm-10">
-								<select name="color" class="form-control" id="color">
-									<option value="">Choose</option>
-									<option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
-									<option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
-									<option style="color:#008000;" value="#008000">&#9724; Green</option>						  
-									<option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
-									<option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
-									<option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
-									<option style="color:#000;" value="#000">&#9724; Black</option>
-
-								</select>
-							</div>
-						</div>
-						<div class="form-group"> 
-							<div class="col-sm-offset-2 col-sm-10">
-								<div class="checkbox">
-									<label class="text-danger"><input type="checkbox"  name="delete"> Delete event</label>
-								</div>
-							</div>
-						</div>
-
-						<input type="hidden" name="id" class="form-control" id="id">
-
-
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="submit" class="btn btn-primary">Save changes</button>
-					</div>
-				</form>
-			</div>
+
+					<input type="hidden" name="id" class="form-control" id="id">
+
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+			</form>
 		</div>
-	</div><!-- container -->
+	</div>
+</div><!-- container -->
 </div>
 
 <!-- jQuery Version 1.11.1 -->
