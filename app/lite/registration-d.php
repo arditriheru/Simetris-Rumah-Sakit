92d51a5291d3f2cd628a3c4f77faf627c6f98d03
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-17 13:45:31 +0700

    Add files via upload

--- app/lite/registration-d.php
+++ app/lite/registration-d.php
@@ -52,83 +52,104 @@
                 while($j = mysqli_fetch_array($i)){
                   $kuota = $j['kuota'];
                 }
-                if($cek==$kuota){
-                  $notifpenuh = "Kuota Penuh";
+                // Query cek jadwal libur
+                $k = mysqli_query($koneksi,
+                  "SELECT COUNT(*) AS cek_libur FROM dokter_jadwal_libur WHERE id_dokter='$id_dokter' AND id_sesi='$id_sesi' AND tanggal='$booking_tanggal';");
+                while($l = mysqli_fetch_array($k)){
+                  $cek_libur      = $l['cek_libur'];
                 }
-                ?>
-                <form action="registration-add-process" method="post" class="">
-                  <div class="form-group">
-                    <label class="control-label mb-1">Nomor Rekam Medik</label>
-                    <input class="form-control" type="text" name="id_catatan_medik"
-                    value="<?php echo $id_catatan_medik; ?>" readonly>
+                if($cek_libur>0){
+                // Cek jadwal libur
+                  echo "<script>
+                  setTimeout(function() {
+                    swal({
+                      title: 'Dokter Cuti',
+                      text: 'Silahkan Re-Schedule',
+                      type: 'error'
+                      }, function() {
+                        window.location = 'javascript: history.back()';
+                        });
+                        }, 10);
+                        </script>";
+                      }else{
+                        if($cek==$kuota){
+                          $notifpenuh = "Kuota Penuh";
+                        }
+                      }
+                      ?>
+                      <form action="registration-add-process" method="post" class="">
+                        <div class="form-group">
+                          <label class="control-label mb-1">Nomor Rekam Medik</label>
+                          <input class="form-control" type="text" name="id_catatan_medik"
+                          value="<?php echo $id_catatan_medik; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <label for="cc-payment" class="control-label mb-1">Nama Pasien</label>
+                          <input class="form-control" type="text" name="nama"
+                          value="<?php echo $nama; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <label for="cc-payment" class="control-label mb-1">Alamat</label>
+                          <input class="form-control" type="text" name="alamat"
+                          value="<?php echo $alamat; ?>" required="">
+                        </div>
+                        <div class="form-group">
+                          <label for="cc-payment" class="control-label mb-1">Kontak</label>
+                          <input class="form-control" type="text" name="kontak"
+                          value="<?php echo $telp; ?>" required="">
+                        </div>
+                        <div class="form-group">
+                          <label class="control-label mb-1">Nama Dokter</label>
+                          <input class="form-control" type="text" name="id_dokter"
+                          value="<?php echo $id_dokter; ?>" hidden>
+                          <input class="form-control" type="text"
+                          value="<?php echo $nama_dokter; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <label class="control-label mb-1">Untuk Tanggal</label>
+                          <input class="form-control" type="text" name='booking_tanggal'
+                          value="<?php echo $booking_tanggal; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <label class="control-label mb-1">Sesi Jam</label>
+                          <input class="form-control" type="text" name='id_sesi'
+                          value="<?php echo $id_sesi; ?>" hidden>
+                          <input class="form-control" type="text" 
+                          value="<?php echo $jam; ?>" readonly>
+                        </div><br>
+                        <div>
+                          <?php if(isset($notifpenuh)){ ?>
+                            <button type="button" class="btn btn-lg btn-danger btn-block">Kuota Penuh</button>
+                            <p align="center" class="blacktext">Silahkan Re-Schedule</p>
+                          <?php }else{ ?>
+                            <p>**Pastikan data sudah terisi dengan benar..</p>
+                            <button id="add" name="add" type="submit" class="btn btn-lg btn-info btn-block">
+                              <span id="search-schedule-amount">Daftar Sekarang</span>
+                              <span id="search-schedule-sending" style="display:none;">Sending…</span>
+                            </button>
+                          <?php } ?>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-                  <div class="form-group">
-                    <label for="cc-payment" class="control-label mb-1">Nama Pasien</label>
-                    <input class="form-control" type="text" name="nama"
-                    value="<?php echo $nama; ?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label for="cc-payment" class="control-label mb-1">Alamat</label>
-                    <input class="form-control" type="text" name="alamat"
-                    value="<?php echo $alamat; ?>" required="">
-                  </div>
-                  <div class="form-group">
-                    <label for="cc-payment" class="control-label mb-1">Kontak</label>
-                    <input class="form-control" type="text" name="kontak"
-                    value="<?php echo $telp; ?>" required="">
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label mb-1">Nama Dokter</label>
-                    <input class="form-control" type="text" name="id_dokter"
-                    value="<?php echo $id_dokter; ?>" hidden>
-                    <input class="form-control" type="text"
-                    value="<?php echo $nama_dokter; ?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label mb-1">Untuk Tanggal</label>
-                    <input class="form-control" type="text" name='booking_tanggal'
-                    value="<?php echo $booking_tanggal; ?>" readonly>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label mb-1">Sesi Jam</label>
-                    <input class="form-control" type="text" name='id_sesi'
-                    value="<?php echo $id_sesi; ?>" hidden>
-                    <input class="form-control" type="text" 
-                    value="<?php echo $jam; ?>" readonly>
-                  </div><br>
-                  <div>
-                    <?php if(isset($notifpenuh)){ ?>
-                      <button type="button" class="btn btn-lg btn-danger btn-block">Kuota Penuh</button>
-                      <p align="center" class="blacktext">Silahkan Re-Schedule</p>
-                    <?php }else{ ?>
-                      <p>**Pastikan data sudah terisi dengan benar..</p>
-                      <button id="add" name="add" type="submit" class="btn btn-lg btn-info btn-block">
-                        <span id="search-schedule-amount">Daftar Sekarang</span>
-                        <span id="search-schedule-sending" style="display:none;">Sending…</span>
-                      </button>
-                    <?php } ?>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div><!-- .card -->
-      </div><!--/.col-->
-    </div><!--/.row-->
-  </div>
-</div>
-<nav class="nav-bottom">
-  <a href="help" class="nav-bottom__link">
-    <i class="material-icons nav-bottom__icon">help_outline</i>
-    <span class="nav-bottom__text">Help</span>
-  </a>
-  <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
-    <i class="material-icons nav__icon">dashboard</i>
-    <span class="nav-bottom__text">Home</span>
-  </a>
-  <a href="javascript: history.back()" class="nav-bottom__link">
-    <i class="material-icons nav-bottom__icon">arrow_back</i>
-    <span class="nav-bottom__text">Back</span>
-  </a>
-</nav>
-<?php include "views/footer.php"; ?> 
+                </div>
+              </div><!-- .card -->
+            </div><!--/.col-->
+          </div><!--/.row-->
+        </div>
+      </div>
+      <nav class="nav-bottom">
+        <a href="help" class="nav-bottom__link">
+          <i class="material-icons nav-bottom__icon">help_outline</i>
+          <span class="nav-bottom__text">Help</span>
+        </a>
+        <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
+          <i class="material-icons nav__icon">dashboard</i>
+          <span class="nav-bottom__text">Home</span>
+        </a>
+        <a href="javascript: history.back()" class="nav-bottom__link">
+          <i class="material-icons nav-bottom__icon">arrow_back</i>
+          <span class="nav-bottom__text">Back</span>
+        </a>
+      </nav>
+      <?php include "views/footer.php"; ?> 
