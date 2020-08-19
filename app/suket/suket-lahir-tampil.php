492b1446b4131a067c3ef4dc7e1225f424def34d
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-19 15:03:15 +0700

    Add files via upload

--- app/suket/suket-lahir-tampil.php
+++ app/suket/suket-lahir-tampil.php
@@ -7,7 +7,7 @@
             <div class="form-group input-group">
               <input type="text" class="form-control" name="id_catatan_medik" placeholder="Pencarian..">
               <span class="input-group-btn">
-                <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Cari</button>
+                <button type="submit" class="btn btn-success"><i class="fa fa-search"></i> Cari</button>
               </span>
             </div>
           </div>
