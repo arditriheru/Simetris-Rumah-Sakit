c98c34e81e932284b3979e55781bcdf6cbf583e4
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-21 13:18:44 +0700

    Add files via upload

--- app/lite/price-list.php
+++ app/lite/price-list.php
@@ -10,16 +10,102 @@
                             <div class="card-body">
                                 <div class="form-group text-center">
                                     <img src="images/logo-rachmi-akreditasi-kars.png" alt="Rachmi Online">
-                                </div>
-                                <hr>
-                                <img class="img-responsive" src="images/price-list.jpg" width="100%" alt="Koneksi internet lambat.."><br>
+                                </div><hr>
+                            </div>
+                        </div>
+                        <!-- Credit Card -->
+                        <div id="pay-invoice">
+                            <div class="card-body">
+                                <ul class="nav nav-pills nav-justified mb-3 mt-2" id="pills-tab" role="tablist">
+                                    <li class="nav-item"> 
+                                        <a class="nav-link active" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="true">Persalinan</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="false">Imunisasi</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content pl-3 pt-2" id="nav-tabContent">
+                                    <div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                                        <img class="img-responsive" src="images/price-list.jpg" width="100%" alt="Koneksi internet lambat.."><br>
+                                        <div align="right"><small>Update : Agustus 2020</small></div>
+                                    </div>
+                                    <div class="tab-pane fade" id="pills-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                      <!-- <div class="card-title">
+                                        <h5 class="text-center">Harga Vaksin</h5>
+                                    </div> -->
+                                    <table class="table table-bordered table-hover table-striped tablesorter">
+                                      <thead>
+                                        <tr>
+                                            <th scope="col"><center>Nama Vaksin</center></th>
+                                            <th scope="col"><center>Harga</center></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <div id="antrian">
+                                            <tr>
+                                                <td><center>Hepatitis A</center></td>
+                                                <td><center>Rp650.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Hepatitis B</center></td>
+                                                <td><center>Rp250.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Imovax/ IPV/ Polio</center></td>
+                                                <td><center>Rp250.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>BCG</center></td>
+                                                <td><center>Rp250.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>IPD/ PCV/ Prevenar</center></td>
+                                                <td><center>Rp950.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Rotarix/ Rotatex</center></td>
+                                                <td><center>Rp450.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Influenza</center></td>
+                                                <td><center>Rp450.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>MR/ Campak</center></td>
+                                                <td><center>Rp450.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Typus</center></td>
+                                                <td><center>Rp600.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Varisela/ Cacar Air</center></td>
+                                                <td><center>Rp650.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Hexaxim</center></td>
+                                                <td><center>Rp1.100.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>Imojev</center></td>
+                                                <td><center>Rp900.000</center></td>
+                                            </tr>
+                                            <tr>
+                                                <td><center>MMR</center></td>
+                                                <td><center>Rp500.000</center></td>
+                                            </tr>
+                                        </div>
+                                    </tbody>
+                                </table><div align="right"><small>Update : Agustus 2020</small></div>
                             </div>
                         </div>
                     </div>
-                </div><!-- .card -->
-            </div><!--/.col-->
-        </div><!--/.row-->
-    </div>
+                </div>
+            </div>
+        </div><!-- .card -->
+    </div><!--/.col-->
+</div><!--/.row-->
+</div>
 </div>
 <nav class="nav-bottom">
   <a href="help" class="nav-bottom__link">
