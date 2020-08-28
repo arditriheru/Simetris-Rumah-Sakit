d2b0f500e6da1969da213e4e8daf993a381446ef
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-08-28 14:49:35 +0700

    Add files via upload

--- app/kuesioner/index.php
+++ app/kuesioner/index.php
@@ -16,16 +16,15 @@
         <form method="post" action="kuesioner-layanan" role="form">
           <div class="row">
             <div class="form-group col-lg-6">
-              <input class="form-control input-lg input-upper" style="height:80px" type="text" name="nama" placeholder="Masukkan Nama Anda" required="">
+              <input class="form-control input-lg input-upper" style="height:70px" type="text" name="nama" placeholder="Masukkan Nama Anda" required="">
             </div>
             <div class="form-group col-lg-6">
-              <input class="form-control input-lg input-upper" style="height:80px" type="text" name="kontak" placeholder="Masukkan Nomor WhatsApp" required="">
+              <input class="form-control input-lg input-upper" style="height:70px" type="text" name="kontak" placeholder="Masukkan Nomor WhatsApp" required="">
             </div>
           </div><br>
           <button type="submit" class="btn btn-block"><img src="images/kuesioner-button.jpg" class="rounded float-right" width="100%"></button>
         </form>
       </div>
     </div>
-  </div>
-</div><br>
-<?php include "views/footer.php"; ?>
+  </div><div align="center"><b>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></b> | All Right Reserved.<a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <b class="bluetext">Tim IT RSKIA Rachmi</b></div><br>
+  <?php include "views/footer.php"; ?>
