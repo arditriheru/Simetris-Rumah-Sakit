b89dc718c0d8153766e6f007dbed5c9b66b05ddc
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-09-01 10:47:14 +0700

    Add files via upload

--- app/kuesioner/ranap/kuesioner-dokter-umum.php
+++ app/kuesioner/ranap/kuesioner-dokter-umum.php
@@ -13,41 +13,53 @@ if(isset($_POST['submitdokterumum'])){
 ?>
 <div class="col-lg-12">
   <div align="center" class="row">
-    <h7><b>4. PELAYANAN</b></h7><br>
-    <h8><b>DOKTER UMUM</b></h8><br><br>
-    <form method="post" action="" role="form">
-      <div class="col-lg-4">
-        <div align="center">
-         <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
-         <input class="form-control" type="hidden" name="skor" value="1">
-         <button type="submit" name="submitdokterumum" class="btn">
-          <img src="../images/a.png" class="rounded float-right" width="400px">
+   <div class="col-lg-2"><br>
+    <a href="javascript:window.history.go(-1);">
+     <img src="../images/kuesioner-previous.png" class="rounded float-right" width="100px">
+   </a>
+ </div>
+ <div class="col-lg-8">
+   <h7><b>4. PELAYANAN</b></h7><br>
+   <h8><b>DOKTER UMUM</b></h8><br><br>
+ </div>
+ <div class="col-lg-2"><br>
+  <a href="#">
+   <img src="../images/kuesioner-next.png" class="rounded float-right" width="100px">
+ </a>
+</div>
+<form method="post" action="" role="form">
+  <div class="col-lg-4">
+    <div align="center">
+     <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
+     <input class="form-control" type="hidden" name="skor" value="1">
+     <button type="submit" name="submitdokterumum" class="btn">
+      <img src="../images/a.png" class="rounded float-right" width="400px">
+    </div>
+    <h2>Sangat Puas</h2>
+  </div>
+</form>
+<form method="post" action="" role="form">
+  <div class="col-lg-4">
+    <div align="center">
+      <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
+      <input class="form-control" type="hidden" name="skor" value="2">
+      <button type="submit" name="submitdokterumum" class="btn">
+        <img src="../images/b.png" class="rounded float-right" width="400px">
+      </div>
+      <h2>Cukup Puas</h2>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-4">
+      <div align="center">
+        <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
+        <input class="form-control" type="hidden" name="skor" value="3">
+        <button type="submit" name="submitdokterumum" class="btn">
+          <img src="../images/d.png" class="rounded float-right" width="400px">
         </div>
-        <h2>Sangat Puas</h2>
+        <h2>Tidak Puas</h2>
       </div>
     </form>
-    <form method="post" action="" role="form">
-      <div class="col-lg-4">
-        <div align="center">
-          <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
-          <input class="form-control" type="hidden" name="skor" value="2">
-          <button type="submit" name="submitdokterumum" class="btn">
-            <img src="../images/b.png" class="rounded float-right" width="400px">
-          </div>
-          <h2>Cukup Puas</h2>
-        </div>
-      </form>
-      <form method="post" action="" role="form">
-        <div class="col-lg-4">
-          <div align="center">
-            <input class="form-control" type="hidden" name="bagian" value="dokter_umum">
-            <input class="form-control" type="hidden" name="skor" value="3">
-            <button type="submit" name="submitdokterumum" class="btn">
-              <img src="../images/d.png" class="rounded float-right" width="400px">
-            </div>
-            <h2>Tidak Puas</h2>
-          </div>
-        </form>
-      </div>
-    </div><br>
-    <?php include "views/footer.php"; ?>
+  </div>
+</div><br>
+<?php include "views/footer.php"; ?>
--- app/kuesioner/ranap/kuesioner-nama-dokter-spesialis.php
+++ app/kuesioner/ranap/kuesioner-nama-dokter-spesialis.php
@@ -19,143 +19,154 @@ if(isset($_POST['submitnamadoktersp'])){
  header("location:kuesioner-parkir");
 }
 ?>
-<div id="page-wrapper">
+<div class="col-lg-12">
   <div align="center" class="row">
-    <div class="col-lg-12">
-      <h7><b>PELAYANAN</b></h7><br>
-      <h8><b>DOKTER SPESIALIS</b></h8><br><br>
-      <div class="row">
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="1">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/soeroyo.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="2">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/irwan.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="3">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/irfan.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="4">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/arsi.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="5">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/umam.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="7">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/restu.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-      </div><br>
-      <div class="row">
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="8">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/marie.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
-        <form method="post" action="" role="form">
-          <div class="col-lg-2">
-            <div align="center">
-              <input class="form-control" type="hidden" name="nama"
-              value="<?php echo $nama; ?>">
-              <input class="form-control" type="hidden" name="kontak"
-              value="<?php echo $kontak; ?>">
-              <input class="form-control" type="hidden" name="pelayanan"
-              value="<?php echo $pelayanan; ?>">
-              <input class="form-control" type="hidden" name="id_dokter" value="9">
-              <button type="submit" name="submitnamadoktersp" class="btn">
-                <img src="../images/akbar.jpg" class="img-rounded" width="100%">
-              </button>
-            </div>
-          </div>
-        </form>
+    <div class="col-lg-2"><br>
+      <a href="javascript:window.history.go(-1);">
+       <img src="../images/kuesioner-previous.png" class="rounded float-right" width="100px">
+     </a>
+   </div>
+   <div class="col-lg-8">
+     <h7><b>PELAYANAN</b></h7><br>
+     <h8><b>DOKTER SPESIALIS</b></h8><br><br>
+   </div>
+   <div class="col-lg-2"><br>
+    <a href="#">
+     <img src="../images/kuesioner-next.png" class="rounded float-right" width="100px">
+   </a>
+ </div>
+</div>
+<div class="row">
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="1">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/soeroyo.jpg" class="img-rounded" width="100%">
+        </button>
       </div>
     </div>
-  </div><br>
-  <?php include "views/footer.php"; ?>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="2">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/irwan.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="3">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/irfan.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="4">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/arsi.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="5">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/umam.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="7">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/restu.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+</div><br>
+<div class="row">
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="8">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/marie.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+  <form method="post" action="" role="form">
+    <div class="col-lg-2">
+      <div align="center">
+        <input class="form-control" type="hidden" name="nama"
+        value="<?php echo $nama; ?>">
+        <input class="form-control" type="hidden" name="kontak"
+        value="<?php echo $kontak; ?>">
+        <input class="form-control" type="hidden" name="pelayanan"
+        value="<?php echo $pelayanan; ?>">
+        <input class="form-control" type="hidden" name="id_dokter" value="9">
+        <button type="submit" name="submitnamadoktersp" class="btn">
+          <img src="../images/akbar.jpg" class="img-rounded" width="100%">
+        </button>
+      </div>
+    </div>
+  </form>
+</div>
+</div><br>
+<?php include "views/footer.php"; ?>
