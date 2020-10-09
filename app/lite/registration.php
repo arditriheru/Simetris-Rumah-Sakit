7b0f4c7d242f4b2478942cf41313223e1ccc5cdc
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-09 08:56:15 +0700

    Add files via upload

--- app/lite/registration.php
+++ app/lite/registration.php
@@ -37,161 +37,141 @@
                  if($cekdata>0){
                   session_start();
                   $_SESSION['id'] = "$id_catatan_medik";
-                  echo "<script>
-                  setTimeout(function() {
-                    swal({
-                      title: 'Yess..',
-                      text: 'Berhasil Login',
-                      type: 'success'
-                      }, function() {
-                        window.location = 'registration-form';
-                        });
-                        }, 10);
-                        </script>";
-                      }else{
-                        echo "<script>
-                        setTimeout(function() {
-                          swal({
-                            title: 'Upss..',
-                            text: 'Nomor RM / Tanggal Lahir Salah',
-                            type: 'error'
-                            }, function() {
-                              window.location = 'javascript:history.back()';
-                              });
-                              }, 10);
-                              </script>";
-                            }
+                  echo "<script>document.location='registration-form'</script>";
+                }else{
+                  echo "<script>alert('Usernama / Password Salah');document.location='javascript: history.back()'</script>";
+                }
+              }
+              ?>
+              <div class="tab-content pl-3 pt-2" id="nav-tabContent">
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                  <div class="card-body card-block">
+                    <form action="queue-check" method="post" role="form">
+                      <div class="form-group">
+                        <label>Nama Dokter</label>
+                        <select class="form-control" type="text" name="id_dokter" required="">
+                          <option value="">Pilih</option>
+                          <?php 
+                          include 'controller/connection.php';
+                          $data = mysqli_query($koneksi,
+                            "SELECT * FROM dokter WHERE status=1;");
+                          while($d = mysqli_fetch_array($data)){
+                            echo "<option value='".$d['id_dokter']."'>".$d['nama_dokter']."</option>";
                           }
                           ?>
-                          <div class="tab-content pl-3 pt-2" id="nav-tabContent">
-                            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                              <div class="card-body card-block">
-                                <form action="queue-check" method="post" role="form">
-                                  <div class="form-group">
-                                    <label>Nama Dokter</label>
-                                    <select class="form-control" type="text" name="id_dokter" required="">
-                                      <option value="">Pilih</option>
-                                      <?php 
-                                      include 'controller/connection.php';
-                                      $data = mysqli_query($koneksi,
-                                        "SELECT * FROM dokter WHERE status=1;");
-                                      while($d = mysqli_fetch_array($data)){
-                                        echo "<option value='".$d['id_dokter']."'>".$d['nama_dokter']."</option>";
-                                      }
-                                      ?>
-                                    </select>
-                                  </div>
-                                  <div class="form-group">
-                                    <label>Jadwal Poli</label>
-                                    <input class="form-control" type="date" name="booking_tanggal" required="" placeholder="Masukkan Jadwal Poli">
-                                  </div>
-                                  <div class="form-group">
-                                    <label>Sesi</label>
-                                    <select class="form-control" type="text" name="id_sesi" required="">
-                                      <option value="">Pilih</option>
-                                      <?php 
-                                      include 'controller/connection.php';
-                                      $data = mysqli_query($koneksi,
-                                        "SELECT * FROM sesi ORDER BY id_sesi ASC;");
-                                      while($d = mysqli_fetch_array($data)){
-                                        echo "<option value='".$d['id_sesi']."'>".$d['nama_sesi']."</option>";
-                                      }
-                                      ?>
-                                    </select>
-                                  </div>
-                                  <div>
-                                    <button id="cekantrian" name="cek" type="submit" class="btn btn-lg btn-info btn-block">
-                                      <span id="search-schedule-amount">Lihat Antrian</span>
-                                      <span id="search-schedule-sending" style="display:none;">Sending…</span>
-                                    </button>
-                                  </div>
-                                </form>
-                              </div>
-                            </div>
-                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                              <div class="col-md-12">
-                                <b class="bluetext">Ketentuan Umum Pendaftaran Online</b><br><br>
-                                <ol>
-                                  <li>Pendaftaran Online sementara ini hanya berlaku bagi Pasien yang telah memiliki Nomor Rekam Medis RSKIA Rachmi yang akan berobat Rawat Jalan.</li>
-                                  <li>Bagi pasien baru yang belum pernah mendaftar di RSKIA Rachmi harap datang langsung ke bagian pendaftaran dengan membawa 1 lembar FC KTP.</li>
-                                  <li>Pendaftaran Online dapat dilakukan untuk kontrol Poli dengan Jadwal H-30 s.d hari H saat jadwal kontrol dilakukan dengan memasukkan : Nomor RM , Tanggal Lahir, Pilihan Hari Kontrol dan Dokter untuk poli reguler. Dokter yang ditunjuk adalah dokter DPJP (Dokter Penanggung Jawab Pelayanan).</li>
-                                  <li>Pasien hanya dapat mendaftar sekali pada Dokter, Jadwal dan Sesi yang sama.</li>
-                                  <li>Jadwal Dokter dapat berubah sewaktu waktu.</li>
-                                  <li>Apabila Anda telah melakukan pendaftaran Online, Anda akan mendapatkan Bukti pendaftaran yang dapat di (screenshot) dan ditunjukkan pada saat Jadwal Kontrol.</li>
-                                  <li>Apabila Anda ingin melihat detail Pendaftaran Online pada sub-menu Cetak Ulang di halaman ini.</li>
-                                  <li>Nomor Antrian Periksa dokter adalah sesuai dengan urutan ketika melakukan registrasi ulang.</li>
-                                  <li>Untuk kasus Gawat Darurat silakan datang ke UGD RSKIA Rachmi.</li>
-                                  <li>Bukti Pendaftaran Online dibawa di loket Pendaftaran RSKIA Rachmi.</li>
-                                  <li>Pasien yang telah melakukan registrasi online diharapkan datang tepat waktu.</li>
-                                </ol>
-                                <b class="bluetext">Ketentuan Khusus</b><br><br>
-                                <ol>
-                                  <li>Pasien dari luar kota wajib membawa hasil Non-Reaktif Rapid Test.</li>
-                                  <li>Periksa Poli Anak pendamping 1 orang.</li>
-                                  <li>Periksa Poli Kandungan pendamping menunggu diluar.</li>
-                                  <li>Selama pandemi tidak melayani USG 4D sampai batas waktu yang belum ditentukan.</li>
-                                </ol><hr>
-                              </div><br>
-                              <div class="card-body card-block">
-                                <form action="" method="post" role="form">
-                                 <div class="form-group">
-                                  <label for="cc-payment" class="control-label mb-1">Nomor Rekam Medik</label>
-                                  <input name="id_catatan_medik" type="text" class="form-control" required="" placeholder="Masukkan Nomor RM Anda">
-                                </div>
-                                <div class="form-group">
-                                  <label for="cc-payment" class="control-label mb-1">Tanggal Lahir</label>
-                                  <input name="tgl_lahir" type="date" class="form-control" required="" placeholder="Masukkan Tanggal Lahir">
-                                </div>
-                                <div>
-                                  <button id="registration" name="ceklogin" type="submit" class="btn btn-lg btn-info btn-block">
-                                    <span id="search-schedule-amount">Login</span>
-                                    <span id="search-schedule-sending" style="display:none;">Sending…</span>
-                                  </button>
-                                </div>
-                              </form>
-                            </div>
-                          </div>
-                          <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                            <div class="card-body card-block">
-                              <form method="post" action="registration-print" role="form">
-                                <div class="form-group">
-                                  <label>Nomor Rekam Medik</label>
-                                  <input class="form-control" type="text" name="id_catatan_medik" required="" placeholder="Masukkan Nomor RM Anda">
-                                </div>
-                                <div class="form-group">
-                                  <label>Jadwal Poli</label>
-                                  <input class="form-control" type="date" name="booking_tanggal" required="" placeholder="Masukkan Jadwal Poli">
-                                </div>
-                                <div>
-                                  <button id="cetak" type="submit" class="btn btn-lg btn-info btn-block">
-                                    <span id="search-schedule-amount">Cetak</span>
-                                    <span id="search-schedule-sending" style="display:none;">Sending…</span>
-                                  </button>
-                                </div>
-                              </form>
-                            </div>
-                          </div>
-                        </div>
+                        </select>
                       </div>
-                    </div>
+                      <div class="form-group">
+                        <label>Jadwal Poli</label>
+                        <input class="form-control" type="date" name="booking_tanggal" required="" placeholder="Masukkan Jadwal Poli">
+                      </div>
+                      <div class="form-group">
+                        <label>Sesi</label>
+                        <select class="form-control" type="text" name="id_sesi" required="">
+                          <option value="">Pilih</option>
+                          <?php 
+                          include 'controller/connection.php';
+                          $data = mysqli_query($koneksi,
+                            "SELECT * FROM sesi ORDER BY id_sesi ASC;");
+                          while($d = mysqli_fetch_array($data)){
+                            echo "<option value='".$d['id_sesi']."'>".$d['nama_sesi']."</option>";
+                          }
+                          ?>
+                        </select>
+                      </div>
+                      <div>
+                        <button id="cekantrian" name="cek" type="submit" class="btn btn-lg btn-info btn-block">
+                          <span id="search-schedule-amount">Lihat Antrian</span>
+                          <span id="search-schedule-sending" style="display:none;">Sending…</span>
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                </div><!-- .card -->
-              </div><!--/.col-->
-            </div><!--/.row-->
+                </div>
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                  <div class="col-md-12">
+                    <b class="bluetext">Ketentuan Umum Pendaftaran Online</b><br><br>
+                    <ol>
+                      <li>Pendaftaran Online sementara ini hanya berlaku bagi Pasien yang telah memiliki Nomor Rekam Medis RSKIA Rachmi yang akan berobat Rawat Jalan.</li>
+                      <li>Bagi pasien baru yang belum pernah mendaftar di RSKIA Rachmi harap datang langsung ke bagian pendaftaran dengan membawa 1 lembar FC KTP.</li>
+                      <li>Pendaftaran Online dapat dilakukan untuk kontrol Poli dengan Jadwal H-30 s.d hari H saat jadwal kontrol dilakukan dengan memasukkan : Nomor RM , Tanggal Lahir, Pilihan Hari Kontrol dan Dokter untuk poli reguler. Dokter yang ditunjuk adalah dokter DPJP (Dokter Penanggung Jawab Pelayanan).</li>
+                      <li>Pasien hanya dapat mendaftar sekali pada Dokter, Jadwal dan Sesi yang sama.</li>
+                      <li>Jadwal Dokter dapat berubah sewaktu waktu.</li>
+                      <li>Apabila Anda telah melakukan pendaftaran Online, Anda akan mendapatkan Bukti pendaftaran yang dapat di (screenshot) dan ditunjukkan pada saat Jadwal Kontrol.</li>
+                      <li>Apabila Anda ingin melihat detail Pendaftaran Online pada sub-menu Cetak Ulang di halaman ini.</li>
+                      <li>Nomor Antrian Periksa dokter adalah sesuai dengan urutan ketika melakukan registrasi ulang.</li>
+                      <li>Untuk kasus Gawat Darurat silakan datang ke UGD RSKIA Rachmi.</li>
+                      <li>Bukti Pendaftaran Online dibawa di loket Pendaftaran RSKIA Rachmi.</li>
+                      <li>Pasien yang telah melakukan registrasi online diharapkan datang tepat waktu.</li>
+                    </ol>
+                    <b class="bluetext">Ketentuan Khusus</b><br><br>
+                    <ol>
+                      <li>Pasien dari luar kota wajib membawa hasil Non-Reaktif Rapid Test.</li>
+                      <li>Periksa Poli Anak pendamping 1 orang.</li>
+                      <li>Periksa Poli Kandungan pendamping menunggu diluar.</li>
+                      <li>Selama pandemi tidak melayani USG 4D sampai batas waktu yang belum ditentukan.</li>
+                    </ol><hr>
+                  </div><br>
+                  <div class="card-body card-block">
+                    <form action="" method="post" role="form">
+                     <div class="form-group">
+                      <label for="cc-payment" class="control-label mb-1">Nomor Rekam Medik</label>
+                      <input name="id_catatan_medik" type="text" class="form-control" required="" placeholder="Masukkan Nomor RM Anda">
+                    </div>
+                    <div class="form-group">
+                      <label for="cc-payment" class="control-label mb-1">Tanggal Lahir</label>
+                      <input name="tgl_lahir" type="date" class="form-control" required="" placeholder="Masukkan Tanggal Lahir">
+                    </div>
+                    <div>
+                      <button id="registration" name="ceklogin" type="submit" class="btn btn-lg btn-info btn-block">
+                        <span id="search-schedule-amount">Login</span>
+                        <span id="search-schedule-sending" style="display:none;">Sending…</span>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div class="card-body card-block">
+                  <form method="post" action="registration-print" role="form">
+                    <div class="form-group">
+                      <label>Nomor Rekam Medik</label>
+                      <input class="form-control" type="text" name="id_catatan_medik" required="" placeholder="Masukkan Nomor RM Anda">
+                    </div>
+                    <div class="form-group">
+                      <label>Jadwal Poli</label>
+                      <input class="form-control" type="date" name="booking_tanggal" required="" placeholder="Masukkan Jadwal Poli">
+                    </div>
+                    <div>
+                      <button id="cetak" type="submit" class="btn btn-lg btn-info btn-block">
+                        <span id="search-schedule-amount">Cetak</span>
+                        <span id="search-schedule-sending" style="display:none;">Sending…</span>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <nav class="nav-bottom">
-          <a href="help" class="nav-bottom__link">
-            <i class="material-icons nav-bottom__icon">help_outline</i>
-            <span class="nav-bottom__text">Help</span>
-          </a>
-          <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
-            <i class="material-icons nav__icon">dashboard</i>
-            <span class="nav-bottom__text">Home</span>
-          </a>
-          <a href="javascript: history.back()" class="nav-bottom__link">
-            <i class="material-icons nav-bottom__icon">arrow_back</i>
-            <span class="nav-bottom__text">Back</span>
-          </a>
-        </nav>
-        <?php include "views/footer.php"; ?> 
+      </div>
+    </div><!-- .card -->
+  </div><!--/.col-->
+</div><!--/.row-->
+</div>
+</div>
+<nav class="nav-bottom">
+  <a href="help" class="nav-bottom__link">
+    <i class="material-icons nav-bottom__icon">help_outline</i>
+    <span class="nav-bottom__text">Help</span>
+  </a>
+  <a href="https://pendaftaran.rskiarachmi.co.id/" class="nav-bottom__link nav-bottom__link--active">
+    <i class="material-icons nav__icon">dashboard</i>
+    <span class="nav-bottom__text">Home</span>
+  </a>
+  <a href="javascript: history.back()" class="nav-bottom__link">
+    <i class="material-icons nav-bottom__icon">arrow_back</i>
+    <span class="nav-bottom__text">Back</span>
+  </a>
+</nav>
+<?php include "views/footer.php"; ?> 
