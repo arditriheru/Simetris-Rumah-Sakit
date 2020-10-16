cdaf5a536bcce5eb47c3641a70dac8868f6df227
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-16 15:53:37 +0700

    Delete pegawai-hapus.php

--- app/sdm/pegawai-hapus.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include '../koneksi.php';
-$id_petugas = $_GET['id_petugas'];
-mysqli_query($koneksi,"delete from psdi_petugas where id_petugas='$id_petugas'");
-header("location:pegawai-tampil");
-?>
