e556e7407352f4814588476952d66ddd8e86d779
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-20 14:55:27 +0700

    Delete user_prosestambah.php

--- app/sdm/user_prosestambah.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include '../koneksi.php';
-$username 	= $_POST['username'];
-$password 	= $_POST['password'];
-$unit 		= $_POST['unit'];
-mysqli_query($koneksi,"insert into admin values('','$username','$password','$unit')");
-header("location:user_tampil.php");
-?>
