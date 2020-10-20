88c860d0176df8d855e9714c243ab1f75c2cc5db
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-20 14:50:44 +0700

    Delete suratkeluarinternal_prosestambah.php

--- app/sdm/suratkeluarinternal_prosestambah.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../koneksi.php';
-$nomor_surat = $_POST['nomor_surat'];
-$tanggal = $_POST['tanggal'];
-$perihal = $_POST['perihal'];
-$tujuan = $_POST['tujuan'];
-mysqli_query($koneksi,"insert into surat_internal values('','$nomor_surat','$tanggal','$perihal','$tujuan')");
-header("location:suratkeluar_tambah.php");
-?>
