db43535449e01538db3b2aec1d328fa0f12098d2
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-20 14:55:16 +0700

    Delete user_prosesedit.php

--- app/sdm/user_prosesedit.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../koneksi.php';
-$id_admin 	= $_GET['id_admin'];
-$unit		= $_POST['unit'];
-$username 	= $_POST['username'];
-$password 	= $_POST['password'];
-mysqli_query($koneksi,"update admin set username='$username', password='$password', unit='$unit' where id_admin='$id_admin'");
-header("location:user_tampil.php");
-?>
