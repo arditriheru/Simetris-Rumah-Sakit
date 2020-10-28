b38840f6c9bd1825efc1a2456cfcb9892d4c4f0c
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-10-28 14:32:59 +0700

    Add files via upload

--- app/manajemen/kuesioner-whatsapp.php
+++ app/manajemen/kuesioner-whatsapp.php
@@ -39,44 +39,44 @@
     ?>
     <nav>
         <div id="wrapper">
-         <!-- Sidebar -->
-         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand">S I M E T R I S</a>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav side-nav">
-            <li><a href="kuesioner-whatsapp.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <i class="fa fa-check-square-o"></i> Kuesioner <b class="caret"></b></a>
+           <!-- Sidebar -->
+           <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand">S I M E T R I S</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li><a href="kuesioner-whatsapp.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-check-square-o"></i> Kuesioner <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="kuesioner-whatsapp.php"><i class="fa fa-check-square-o"></i> Lihat</a></li>
+                            <li><a href="kuesioner-whatsapp.php"><i class="fa fa-check-square-o"></i> WhatsApp</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right navbar-user">
+                  <li class="dropdown user-dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-circle"></i> <?php echo $nama_login;?>&nbsp;<b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="kuesioner-whatsapp.php"><i class="fa fa-check-square-o"></i> Lihat</a></li>
-                        <li><a href="kuesioner-whatsapp.php"><i class="fa fa-check-square-o"></i> WhatsApp</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right navbar-user">
-              <li class="dropdown user-dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-circle"></i> <?php echo $nama_login;?>&nbsp;<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li class="divider"></li>
-                  <li>
-                    <a href="logout.php"><i class="fa fa-power-off">
-                    </i> Log Out</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</div><!-- /.navbar-collapse -->
+                      <li class="divider"></li>
+                      <li>
+                        <a href="logout.php"><i class="fa fa-power-off">
+                        </i> Log Out</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
 </nav>
 <div id="page-wrapper">
   <div class="row">
@@ -126,8 +126,8 @@ $nm = $_GET['nm'];
 </form>
 </div>
 <div class="col-lg-4">
- <?php
- if(isset($_POST['submitwa'])){
+   <?php
+   if(isset($_POST['submitwa'])){
     $sub_kontak = $_POST['nomor_wa'];
     $nomor_wa   = substr($sub_kontak,1);
 }
@@ -175,23 +175,22 @@ $nm = $_GET['nm'];
   <?php 
   $no = 1;
   if(!isset($rm) && !isset($nm)){
-   $data = mysqli_query($koneksi,
-    "SELECT ksr_trn.id_catatan_medik, mr_pasien.nama, mr_pasien.telp, ksr_trn.posting
-    FROM ksr_trn
-    JOIN mr_pasien
-    ON ksr_trn.id_catatan_medik = mr_pasien.id_catatan_medik
-    WHERE ksr_trn.posting IS NULL
-    GROUP BY ksr_trn.id_catatan_medik
-    ORDER BY ksr_trn.id_ksr_trn DESC
-    LIMIT 50;");
-}elseif(isset($rm)){
+     $data = mysqli_query($koneksi,
+        "SELECT ksr_trn.id_catatan_medik, mr_pasien.nama, mr_pasien.telp
+        FROM ksr_trn
+        JOIN mr_pasien
+        ON ksr_trn.id_catatan_medik = mr_pasien.id_catatan_medik
+        GROUP BY ksr_trn.id_catatan_medik
+        ORDER BY ksr_trn.id_ksr_trn DESC
+        LIMIT 50;");
+ }elseif(isset($rm)){
     $data = mysqli_query($koneksi,
         "SELECT id_catatan_medik, nama, telp
         FROM mr_pasien
         WHERE id_catatan_medik = '$rm'
         GROUP BY id_catatan_medik;");
 }elseif(isset($nm)){
-   $data = mysqli_query($koneksi,
+ $data = mysqli_query($koneksi,
     "SELECT id_catatan_medik, nama, telp
     FROM mr_pasien
     WHERE nama LIKE '%' '$nm' '%'
