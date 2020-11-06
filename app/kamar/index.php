c14e360be81eea27904641703252bf38530e28a0
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-11-06 10:15:29 +0700

    Add files via upload

--- app/kamar/index.php
+++ app/kamar/index.php
@@ -50,14 +50,20 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="table-responsive">
-              <table class="table table-bordered table-hover table-striped tablesorter">
+              <table class="table">
+                <div class="col-lg-6">
+                  <div style="background-color: #ffffff; height: auto; margin: 10px 0px; padding: 5px; text-align: center; width: auto;">
+                  Kosong</div>
+                </div>
+                <div class="col-lg-6">
+                  <div style="background-color: #fa7f72; height: auto; margin: 10px 0px; padding: 5px; text-align: center; width: auto;">
+                  Terpakai</div>
+                </div>
                 <thead>
                   <tr>
-                    <th><center>No</center></th>
-                    <th><center>Kamar</center></th>
-                    <th><center>Kelas</center></th>
-                    <th><center>Bed</center></th>
-                    <th><center>Keterangan</center></th>
+                    <th scope="col"><center>Kelas</center></th>
+                    <th scope="col"><center>Kamar</center></th>
+                    <th scope="col"><center>Bed</center></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -65,332 +71,45 @@
                   <?php 
                   $no = 1;
                   $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
+                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.ket_antri,
+                   IF(mr_tt.no_bed='1', 'A', 'B') AS bed
                    FROM mr_tt, mr_unit
                    WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 6;");
+                   AND mr_tt.id_unit
+                   IN ('6','29','24','26','7','28','27','31','30','25')
+                   ORDER BY mr_unit.nama_unit ASC;");
                   while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
+                    $ket_antri = $d['ket_antri'];
                     ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center><font class="bluetext"><?php echo $d['kelas'];?></font></center></td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                  <!---------- Batas ----------->
-                  <?php 
-                  $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                   FROM mr_tt, mr_unit
-                   WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 29;");
-                  while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
-                    ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center><font class="bluetext"><?php echo $d['kelas'];?></center></font></td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                  <!---------- Batas ----------->
-                  <?php 
-                  $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                   FROM mr_tt, mr_unit
-                   WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 24;");
-                  while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
-                    ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center><font class="bluetext"><?php echo $d['kelas'];?></center></font></td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                  <!---------- Batas ----------->
-                  <?php 
-                  $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                   FROM mr_tt, mr_unit
-                   WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 26;");
-                  while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
-                    ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center><font class="bluetext"><?php echo $d['kelas'];?></center></font></td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                  <!---------- Batas ----------->
-                  <?php 
-                  $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                   FROM mr_tt, mr_unit
-                   WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 7;");
-                  while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
-                    ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center><font class="bluetext"><?php echo $d['kelas'];?></center></font></td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                  <!---------- Batas ----------->
-                  <?php 
-                  $data = mysqli_query($koneksi,
-                   "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                   FROM mr_tt, mr_unit
-                   WHERE mr_tt.id_unit = mr_unit.id_unit
-                   AND mr_tt.id_unit = 28;");
-                  while($d = mysqli_fetch_array($data)){
-                    $id_register = $d['id_register'];
-                    ?>
-                    <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center>
-                        <font class="bluetext">Kelas <?php echo $d['kelas'];?></center></font>
-                      </td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></td>
-                        <td><center>
-                          <?php
-                          if($id_register=='123'){
-                            ?>
-                            <font class="greentext">Kosong</font>
-                            <?php
-                          }else{
-                            ?>
-                            <font class="redtext">Terpakai</font>
-                            <?php
-                          }
-                          ?>
-                        </center></td>
-                      </tr>
-                    <?php } ?>
-                    <!---------- Batas ----------->
-                    <?php 
-                    $data = mysqli_query($koneksi,
-                     "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                     FROM mr_tt, mr_unit
-                     WHERE mr_tt.id_unit = mr_unit.id_unit
-                     AND mr_tt.id_unit = 27;");
-                    while($d = mysqli_fetch_array($data)){
-                      $id_register = $d['id_register'];
-                      ?>
+                    <?php
+                    if($ket_antri=='3'){ ?>
                       <tr>
-                        <td><center><?php echo $no++; ?></center></td>
-                        <td><left><?php echo $d['nama_unit']; ?></left></td>
-                        <td><center>
-                          <font class="bluetext">Kelas <?php echo $d['kelas'];?></center></font>
-                        </td>
-                        <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                        <td><center>
-                          <?php
-                          if($id_register=='123'){
-                            ?>
-                            <font class="greentext">Kosong</font>
-                            <?php
-                          }else{
-                            ?>
-                            <font class="redtext">Terpakai</font>
-                            <?php
-                          }
-                          ?>
-                        </center></td>
-                      </tr>
-                    <?php } ?>
-                    <!---------- Batas ----------->
-                    <?php 
-                    $data = mysqli_query($koneksi,
-                     "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                     FROM mr_tt, mr_unit
-                     WHERE mr_tt.id_unit = mr_unit.id_unit
-                     AND mr_tt.id_unit = 31;");
-                    while($d = mysqli_fetch_array($data)){
-                      $id_register = $d['id_register'];
-                      ?>
-                      <tr>
-                        <td><center><?php echo $no++; ?></center></td>
-                        <td><left><?php echo $d['nama_unit']; ?></left></td>
-                        <td><center>
-                          <font class="bluetext">Kelas <?php echo $d['kelas'];?></center></font>
-                        </td>
-                        <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                        <td><center>
-                          <?php
-                          if($id_register=='123'){
-                            ?>
-                            <font class="greentext">Kosong</font>
-                            <?php
-                          }else{
-                            ?>
-                            <font class="redtext">Terpakai</font>
-                            <?php
-                          }
-                          ?>
-                        </center></td>
-                      </tr>
-                    <?php } ?>
-                    <!---------- Batas ----------->
-                    <?php 
-                    $data = mysqli_query($koneksi,
-                     "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                     FROM mr_tt, mr_unit
-                     WHERE mr_tt.id_unit = mr_unit.id_unit
-                     AND mr_tt.id_unit = 30;");
-                    while($d = mysqli_fetch_array($data)){
-                      $id_register = $d['id_register'];
-                      ?>
-                      <tr>
-                        <td><center><?php echo $no++; ?></center></td>
-                        <td><left><?php echo $d['nama_unit']; ?></left></td>
-                        <td><center>
-                          <font class="bluetext">Kelas <?php echo $d['kelas'];?></center></font>
-                        </td>
-                        <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                        <td><center>
-                          <?php
-                          if($id_register=='123'){
-                            ?>
-                            <font class="greentext">Kosong</font>
-                            <?php
-                          }else{
-                            ?>
-                            <font class="redtext">Terpakai</font>
-                            <?php
-                          }
-                          ?>
-                        </center></td>
-                      </tr>
-                    <?php } ?>
-                    <!---------- Batas ----------->
-                    <?php 
-                    $data = mysqli_query($koneksi,
-                     "SELECT mr_tt.kelas, mr_unit.nama_unit, mr_tt.no_bed, mr_tt.id_register
-                     FROM mr_tt, mr_unit
-                     WHERE mr_tt.id_unit = mr_unit.id_unit
-                     AND mr_tt.id_unit = 25
-                     ORDER BY mr_tt.no_bed ASC;");
-                    while($d = mysqli_fetch_array($data)){
-                     $id_register = $d['id_register'];
-                     ?>
-                     <tr>
-                      <td><center><?php echo $no++; ?></center></td>
-                      <td><left><?php echo $d['nama_unit']; ?></left></td>
-                      <td><center>
-                        <font class="bluetext">Kelas <?php echo $d['kelas'];?></center></font>
-                      </td>
-                      <td><center>Bed <?php echo $d['no_bed']; ?></center></td>
-                      <td><center>
-                        <?php
-                        if($id_register=='123'){
-                          ?>
-                          <font class="greentext">Kosong</font>
-                          <?php
-                        }else{
-                          ?>
-                          <font class="redtext">Terpakai</font>
-                          <?php
-                        }
-                        ?>
-                      </center></td>
-                    </tr>
-                  <?php } ?>
-                </tbody>
-              </table>
-            </div>
-          </div><br>
-          <?php include"../../system/copyright.php"?>
-          <!-- /.row -->
-          <br><br>
-        </div><!-- /#page-wrapper -->
-      </div><!-- /#wrapper -->
-      <!-- JavaScript -->
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/font-awesome.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/jquery-1.10.2.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/bootstrap.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/sweetalert.min.js"></script>
-      <!-- Page Specific Plugins -->
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/chartjs/Chart.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/morris/chart-data-morris.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/tablesorter/jquery.tablesorter.js"></script>
-      <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/tablesorter/tables.js"></script>
-    </body>
-    </html>
+                      <?php }else{ ?>
+                        <tr style="background-color: #fa7f72;">
+                        <?php } ?>
+                        <td><center><?php echo $d['kelas'];?></center></td>
+                        <td><center><?php echo $d['nama_unit']; ?></center></td>
+                        <td><center><?php echo $d['bed']; ?></td>
+                        </tr>
+                      <?php } ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div><br>
+              <?php include"../../system/copyright.php"?>
+              <!-- /.row -->
+              <br><br>
+            </div><!-- /#page-wrapper -->
+          </div><!-- /#wrapper -->
+          <!-- JavaScript -->
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/font-awesome.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/jquery-1.10.2.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/bootstrap.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/sweetalert.min.js"></script>
+          <!-- Page Specific Plugins -->
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/chartjs/Chart.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/morris/chart-data-morris.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/tablesorter/jquery.tablesorter.js"></script>
+          <script type="text/javascript" src="https://pendaftaran.rskiarachmi.co.id/vendors/js/tablesorter/tables.js"></script>
+        </body>
+        </html>
